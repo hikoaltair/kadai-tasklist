cffac055a9a0807df65aaa9e59899c6cec76161d
mrazniwaka <niwakasizin@gmail.com>
2020-08-08 08:48:36 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+//CRUD
+//メッセージの個別詳細ページ表示
+//Route::get('tasks/{id}','TasksController@show');
+
+//メッセージの新規登録処理
+//Route::post('tasks','TasksController@store');
+
+//メッセージの更新処理
+//Route::put('tasks/{id}','TasksController@update');
+
+//メッセージを削除
+//Route::delete('tasks/{id}','TasksController@destroy');
+
+//showの補助ページ
+//Route::get('tasks','TasksController@index')->name('tasks.index');
+
+//create 新規作成用のフォームページ
+//Route::get('tasks/create','TasksController@create')->name('tasks.create');
+
+//編集ページ
+//Route::get('tasks/{id}/edit','TasksController@edit')->name('tasks.edit');
+
+Route::resource('tasks','TasksController');
