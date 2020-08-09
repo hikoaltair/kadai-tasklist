026c2b2e477038cc929cf23489beb279e5da942a
mrazniwaka <niwakasizin@gmail.com>
2020-08-09 11:06:40 +0000

    add column

---
+++ database/migrations/2020_08_09_192945_add_status_to_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('status',10); //statusをテーブルに追加（最大文字数を１０に指定）
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,6 +9,7 @@
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>ステータス</th>
                     <th>タスク</th>
                 </tr>
             </thead>
@@ -16,6 +17,7 @@
                 @foreach ($tasks as $task)
                 <tr>
                     <td>{{ $task->id }}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
@@ -23,8 +25,8 @@
         </table>
     @endif
     
-    {{-- タスク作成ページへのリンク　--}}
-    {!! link_to_route('tasks.create','新規タスクの投稿',[], ['class'=>'btn btn-primary'])  !!}
+     {{-- タスク作成ページへのリンク　--}}
+     {!! link_to_route('tasks.create','新規タスクの投稿',[], ['class'=>'btn btn-primary'])  !!}
     
     <br>
     <br>
