6b534b5cf827b15477be849664c243cdec929595
mrazniwaka <niwakasizin@gmail.com>
2020-08-17 08:58:08 +0000

    user registration and login

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,12 @@ Route::get('/', 'TasksController@index');
 //Route::get('tasks/{id}/edit','TasksController@edit')->name('tasks.edit');
 
 Route::resource('tasks','TasksController');
+
+//ユーザ登録
+Route::get('signup','Auth\RegisterController@showRegstrationForm')->name('signup.get');
+Route::post('signup','Auth\RegisterController@register')->name('signup.post');
+
+// 認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
